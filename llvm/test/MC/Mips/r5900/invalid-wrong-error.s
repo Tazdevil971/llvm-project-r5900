235b7277e7b65478cf74716711e8ac475790ce24
Davide Mor <tazdevil971@gmail.com>
2024-09-22 23:33:02 +0200

    [r5900] Fixed tests after rebase

--- llvm/test/MC/Mips/r5900/invalid-wrong-error.s
+++ llvm/test/MC/Mips/r5900/invalid-wrong-error.s
@@ -4,30 +4,8 @@
 # RUN: FileCheck %s < %t1
 
 foo:
-    # COP2 has qword sized registers
-    swc2 $2, 0($2)  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    lwc2 $2, 0($2)  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    ldc2 $2, 0($2)  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    sdc2 $2, 0($2)  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    
     # COP3 is not present
     mfc3 $2, $2, 0  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: unknown instruction, did you mean: mfc0, mfc2?
     mtc3 $2, $2, 0  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: unknown instruction, did you mean: mtc0, mtc2?
-    swc3 $2, 0($2)  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    lwc3 $2, 0($2)  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    ldc3 $2, 0($2)  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    sdc3 $2, 0($2)  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
     dmfc3 $2, $2, 0 # CHECK: :[[@LINE]]:{{[0-9]+}}: error: unknown instruction, did you mean: dmfc0, dmfc2?
     dmtc3 $2, $2, 0 # CHECK: :[[@LINE]]:{{[0-9]+}}: error: unknown instruction, did you mean: dmtc0, dmtc2?
-
-    # R5900 is single core
-    ll $2, 0($2)  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    sc $2, 0($2)  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    lld $2, 0($2) # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    scd $2, 0($2) # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-
-    # R5900 does not support ll/sc
-    ll $2, 0($2)  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    sc $2, 0($2)  # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    lld $2, 0($2) # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
-    scd $2, 0($2) # CHECK: :[[@LINE]]:{{[0-9]+}}: error: invalid operand for instruction
