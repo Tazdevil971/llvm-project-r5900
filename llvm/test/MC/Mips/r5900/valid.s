beabeae75eac95108cae8346e11b5d6090da7de2
Davide Mor <tazdevil971@gmail.com>
2024-10-21 17:36:07 +0200

    [r5900] Added conditional moves instructions

--- llvm/test/MC/Mips/r5900/valid.s
+++ llvm/test/MC/Mips/r5900/valid.s
@@ -9,5 +9,11 @@ foo:
     sync.l # CHECK: sync    # encoding: [0x00,0x00,0x00,0x0f]
     sync.p # CHECK: sync.p  # encoding: [0x00,0x00,0x04,0x0f]
 
+    # Quadword instructions
     lq $2, 100($3) # CHECK: lq $2, 100($3) # encoding: [0x78,0x62,0x00,0x64]
     sq $2, 100($3) # CHECK: sq $2, 100($3) # encoding: [0x7c,0x62,0x00,0x64]
+
+    # Instructions backported from MIPS4
+    pref 0, 100($3) # CHECK: pref 0, 100($3) # encoding: [0xcc,0x60,0x00,0x64]
+    movn $2, $3, $4 # CHECK: movn $2, $3, $4 # encoding: [0x00,0x64,0x10,0x0b]
+    movz $2, $3, $4 # CHECK: movz $2, $3, $4 # encoding: [0x00,0x64,0x10,0x0a]
